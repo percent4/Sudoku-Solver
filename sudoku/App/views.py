6cb1201c81dce167d14c281f94f0734beb824270
Jclian <33828982+percent4@users.noreply.github.com>
2018-01-10 16:08:28 +0800

    Update views.py

--- sudoku/App/views.py
+++ sudoku/App/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response
 def index(request):
     return render_to_response('index.html')
 
-#Read a Sudoku puzzle from the keyboard
+#Read a Sudoku puzzle from index.html page
 def readAPuzzle(lst):
     grid=[]
     for i in range(9):
